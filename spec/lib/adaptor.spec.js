6ec102bca93d555261a413340142020d0e71231d
edgarsilva <edgar.osc@gmail.com>
2014-12-11 19:44:15 -0600

    First commit.

---
+++ spec/lib/adaptor.spec.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var Imp = source("adaptor");
+
+describe("Cylon.Adaptors.Imp", function() {
+  var adaptor = new Imp();
+
+  it("needs tests");
+});
---
+++ spec/lib/driver.spec.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var Imp = source("driver");
+
+describe("Cylon.Drivers.Imp", function() {
+  var driver = new Imp({
+    connection: {}
+  });
+
+  it("needs tests");
+});
